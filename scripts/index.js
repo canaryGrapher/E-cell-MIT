7afb2d1cbf7dfa2a7b454084d80e7e715ed875ae
Yash Aryan <yasharyan307@outlook.com>
2021-04-15 11:57:46 +0530

    Completed team design

--- scripts/index.js
+++ scripts/index.js
@@ -21,14 +21,22 @@ const getTeamMembers = async() => {
     const parsedMemberData = await teamMemberData.json();
     for (let dummy_incremator = parsedMemberData.length - 1; dummy_incremator >= 0; dummy_incremator--) {
         // checking the availability of social links
-        const insta = parsedMemberData[dummy_incremator].instagram ? `<a href="${parsedMemberData[dummy_incremator].instagram}" target="_blank"><i class="fab fa-instagram"></i></a>` : null;
+        const phone = parsedMemberData[dummy_incremator].phone ? `<a href="${parsedMemberData[dummy_incremator].phone}" target="_blank"><i class="fas fa-phone mx-2"></i></a>` : "";
+        const email = parsedMemberData[dummy_incremator].email ? `<a href="${parsedMemberData[dummy_incremator].phone}" target="_blank"><i class="fas fa-envelope mx-2"></i></a>` : "";
+        const instagram = parsedMemberData[dummy_incremator].instagram ? `<a href="${parsedMemberData[dummy_incremator].instagram}" target="_blank"><i class="fab fa-instagram mx-2"></i></a>` : "";
+        const facebook = parsedMemberData[dummy_incremator].facebook ? `<a href="${parsedMemberData[dummy_incremator].facebook}" target="_blank"><i class="fab fa-facebook-f mx-2"></i></a>` : "";
+        const linkedin = parsedMemberData[dummy_incremator].linkedin ? `<a href="${parsedMemberData[dummy_incremator].lnkedin}" target="_blank"><i class="fab fa-linkedin-in mx-2"></i></a>` : "";
+
         let memberTemplate = `
         <div class="col-3 member py-4">
             <img class="img rounded-circle mx-auto" src="${parsedMemberData[dummy_incremator].imgURL}" alt="Dhwanit Shah" height="250" />
             <p class="name-tag py-2">${parsedMemberData[dummy_incremator].name}</p>
             <div class="d-flex flex-row justify-content-center member-social-links">
-                <i class="fas fa-envelope mx-3"></i>
-                <i class="fas fa-phone mx-3"></i>
+                ${phone}
+                ${email}
+                ${instagram}
+                ${facebook}
+                ${linkedin}
             </div>
         </div>`
         innerHTML_container += memberTemplate;
