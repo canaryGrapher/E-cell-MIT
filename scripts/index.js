bf738dd3b2c88c3448c306fc17b8f6a9b3c85db1
Yash Aryan <yasharyan307@outlook.com>
2021-04-15 12:02:28 +0530

    Updated json document

--- scripts/index.js
+++ scripts/index.js
@@ -19,7 +19,7 @@ const getTeamMembers = async() => {
     let innerHTML_container = "";
     const teamMemberData = await fetch('https://raw.githubusercontent.com/canaryGrapher/E-cell-MIT/master/scripts/data/team.json');
     const parsedMemberData = await teamMemberData.json();
-    for (let dummy_incremator = parsedMemberData.length - 1; dummy_incremator >= 0; dummy_incremator--) {
+    for (let dummy_incremator = 0; dummy_incremator < parsedMemberData.length; dummy_incremator++) {
         // checking the availability of social links
         const phone = parsedMemberData[dummy_incremator].phone ? `<a href="${parsedMemberData[dummy_incremator].phone}" target="_blank"><i class="fas fa-phone mx-2"></i></a>` : "";
         const email = parsedMemberData[dummy_incremator].email ? `<a href="${parsedMemberData[dummy_incremator].phone}" target="_blank"><i class="fas fa-envelope mx-2"></i></a>` : "";
@@ -31,6 +31,7 @@ const getTeamMembers = async() => {
         <div class="col-3 member py-4">
             <img class="img rounded-circle mx-auto" src="${parsedMemberData[dummy_incremator].imgURL}" alt="Dhwanit Shah" height="250" />
             <p class="name-tag py-2">${parsedMemberData[dummy_incremator].name}</p>
+            <p class="text-white">Position Undefined</p>
             <div class="d-flex flex-row justify-content-center member-social-links">
                 ${phone}
                 ${email}
